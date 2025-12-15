d7becacf1438e31de9fc670a578db13cc862ab6d
Shay Hatan <ShayHa@cyber.org.il>
2025-12-15 19:40:16 +0200

    feat: trees - same binary tree

---
+++ trees/same_binary_tree/solution.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+using namespace std;
+
+/*
+ * Same Binary Tree
+ *
+ * DFS (Preorder):
+ * - compare current nodes
+ * - recurse left
+ * - recurse right
+ *
+ * Time:  O(n)
+ * Space: O(h)
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p || !q) return p == q;
+        if (p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+};
