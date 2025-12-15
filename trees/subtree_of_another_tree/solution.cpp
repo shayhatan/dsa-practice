db12b9ce2f5805a86fc4785b02245f769c98e020
Shay Hatan <ShayHa@cyber.org.il>
2025-12-15 19:59:55 +0200

    feat: trees - subtree of another tree

---
+++ trees/subtree_of_another_tree/solution.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+using namespace std;
+
+/*
+ * Subtree of Another Tree
+ *
+ * DFS Preorder
+ *
+ * Time:  O(n * m)
+ * Space: O(h)
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p || !q) return p == q;
+        if (p->val != q->val) return false;
+
+        return isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right);
+    }
+
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root) return false;
+
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+
+        return isSubtree(root->left, subRoot) ||
+               isSubtree(root->right, subRoot);
+    }
+};
