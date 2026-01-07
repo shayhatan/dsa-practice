c5a476620cc25a5abc57461c8a91e805f0dbccdf
Shay Hatan <ShayHa@cyber.org.il>
2026-01-07 11:01:25 +0200

    feat: two_pointers - two_sum_ii

---
+++ two_pointers/two_sum_ii/solution.cpp
@@ -0,0 +1,22 @@
+// Two pointers on sorted array.
+// Time: O(n), Space: O(1)
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+
+            if (sum == target) return {l + 1, r + 1};
+            if (sum < target) ++l;
+            if (sum > target) --r;
+        }
+
+        return {};
+    }
+};
